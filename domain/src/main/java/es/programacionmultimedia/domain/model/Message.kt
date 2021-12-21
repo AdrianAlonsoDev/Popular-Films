4e3f72ff399c9f00e26fd637e9044995cad5b550
AdrianArtimez <adrianartimez@outlook.com>
2021-12-21 17:27:45 +0100

    Re-updated

--- domain/src/main/java/es/programacionmultimedia/domain/model/Message.kt
+++
@@ -1,7 +0,0 @@
-package es.programacionmultimedia.domain.model
-
-
-data class Message (
-    val message : String,
-    val messageType: MessageType
-)
--- domain/src/main/java/es/programacionmultimedia/domain/model/MessageType.kt
+++
@@ -1,6 +0,0 @@
-package es.programacionmultimedia.domain.model
-
-
-enum class MessageType {
-    NORMAL, ERROR, NETWORKERROR
-}

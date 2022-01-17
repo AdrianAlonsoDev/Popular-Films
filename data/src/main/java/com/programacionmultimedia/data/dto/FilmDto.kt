65e2489c9583a290cb9df6e9ce5de90d2b416aac
adrianartimez <Chumiligithub1>
2022-01-17 19:58:58 +0100

    Ejercicio 9.3

---
+++ data/src/main/java/com/programacionmultimedia/data/dto/FilmDto.kt
@@ -0,0 +1,11 @@
+package com.programacionmultimedia.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class FilmDto(
+    @SerializedName("id") val id:Int,
+    @SerializedName("title") val title: String,
+    @SerializedName("overview")val description: String,
+    @SerializedName("vote_average") val rating: Double,
+    @SerializedName("poster_path") val imageUrl: String
+)
